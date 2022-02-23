716fe06b3db653a4d1f787dfe08b51aac8385212
Hannah Bregman <hannah.b@myplanet.com>
2022-02-23 11:02:28 -0500

    ticket created refactor

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,5 +1,5 @@
 import { Listener } from './base-listener'
-import { Message, Stan } from 'node-nats-streaming'
+import { Message } from 'node-nats-streaming'
 import { TicketCreatedEvent } from './ticket-created-event'
 import { Subjects } from './subjects'
 
@@ -7,7 +7,7 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated
   queueGroupName = 'payments-service'
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data)
     msg.ack()
   }
